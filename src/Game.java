49a07209261a55efdf472784fed0d0a6097c7c9b
jackedwards1128 <jack.edwards@menloschool.org>
2024-11-22 13:44:37 -0800

    Added betting and revealing middle cards (flop, turn, river)

--- src/Game.java
+++ src/Game.java
@@ -6,17 +6,18 @@ public class Game {
 
     private int playerCount;
     private Deck pile;
+    private int bet;
+    private ArrayList<Card> middleCards;
     private ArrayList<Player> players;
-
-    public static void main(String[] args) {
-        Game poker = new Game();
-    }
 
     public Game() {
         players = new ArrayList<Player>();
         Deck deck = new Deck();
         deck.shuffle();
         this.pile = deck;
+
+        middleCards = new ArrayList<Card>();
+
         this.beginGame();
     }
 
@@ -52,6 +53,19 @@ public class Game {
 
         showHands();
 
+        // flop
+        flop();
+        gatherBets(0);
+
+        // turn
+        revealCard();
+        showCards();
+        gatherBets(0);
+
+        // river
+        revealCard();
+        showCards();
+        gatherBets(0);
     }
 
     private void showHands() {
@@ -72,8 +86,85 @@ public class Game {
         }
     }
 
+    private void flop() {
+        for(int i = 0; i < 3; i++) {
+            revealCard();
+        }
+        showCards();
+    }
+
+    private void showCards() {
+        System.out.print("//////// MIDDLE CARDS: ");
+
+        for(int i = 0; i < middleCards.size(); i++) {
+            System.out.print(middleCards.get(i).toString());
+            if (i != middleCards.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println(" ////////");
+    }
+
+    private void revealCard() {
+        middleCards.add(pile.remove(0));
+    }
+
+    private void gatherBets(int startingSpot) {
+        for(int i = startingSpot; i < playerCount; i++) {
+            char choice = '0';
+
+            while (choice != 'c' && choice != 'r' && choice != 'f') {
+                System.out.println(players.get(i).getName() + ", do you want to (f)old, (c)heck/call, or (r)aise?");
+                Scanner input = new Scanner(System.in);
+                choice = input.nextLine().charAt(0);
+            }
+
+            boolean playerFolded = false;
+            switch (choice) {
+                case 'f':
+                    System.out.println(players.get(i).getName() + " folded out of the round");
+                    players.remove(i);
+                    playerCount--;
+                    playerFolded = true;
+                    i--;
+                    break;
+                case 'c':
+                    players.get(i).setBet(bet);
+                    System.out.println(players.get(i).getName() + " matched to a bet of $" + bet);
+                    break;
+                case 'r':
+                    int newBet = -1;
+
+                    while (newBet <= bet) {
+                        System.out.println("what would you like to raise the bet to? ");
+                        Scanner input = new Scanner(System.in);
+                        newBet = input.nextInt();
+                    }
+
+                    bet = newBet;
+                    players.get(i).setBet(bet);
+                    System.out.println(players.get(i).getName() + " raise the a bet to $" + bet);
+                    break;
+            }
+        }
+        for(int k = 0; k < playerCount; k++) {
+            if(players.get(k).getBet() != bet) {
+                gatherBets(k);
+            }
+        }
+    }
+
+    private
+
     private static void printInstructions() {
         System.out.println("WELCOME TO POKER wahwahwahh");
+    }
+
+
+
+    public static void main(String[] args) {
+        Game poker = new Game();
     }
 }
 
--- src/Player.java
+++ src/Player.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 public class Player {
     private ArrayList<Card> hand;
     private String name;
+    private int bet;
+    private int rank;
     private int order;
 
     public Player(String name, int order) {
@@ -19,11 +21,26 @@ public class Player {
         return hand.get(0).toString() + " and a " + hand.get(1).toString();
     }
 
+    public int getBet() {
+        return bet;
+    }
+
+    public void setBet(int bet) {
+        this.bet = bet;
+    }
+
     public String getName() {
         return name;
     }
 
     public int getOrder() {
         return order;
+    }
+
+    public void determineRank() {
+        int[] frequency = new int[13];
+        int[] pairs = new int[3];
+
+
     }
 }
